6eba388d32b8939aa60a7bacfffeb7bae674312a
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-17 13:13:58 -0600

    getting middleware flushed out

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,5 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
-
+const { findBy } = require('../users/users-model');
 const restricted = (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header:
@@ -34,7 +34,7 @@ const only = role_name => (req, res, next) => {
 }
 
 
-const checkUsernameExists = (req, res, next) => {
+const checkUsernameExists = async (req, res, next) => {
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -42,29 +42,22 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
- next()
+ try{
+  const [user] = await findBy(req.body.username)
+    if (!user) {
+      next({ status: 401, message: 'message": "Invalid credentials'})
+    } else {
+      next()
+    }
+ }
+ catch(err){
+   next(err)
+ }
 }
 
 
 const validateRoleName = (req, res, next) => {
-  /*
-    If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
-
-    If role_name is missing from req.body, or if after trimming it is just an empty string,
-    set req.role_name to be 'student' and allow the request to proceed.
-
-    If role_name is 'admin' after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be admin"
-    }
-
-    If role_name is over 32 characters after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be longer than 32 chars"
-    }
-  */
+ 
  if(!req.body.role_name || !req.body.role_name.trim()) {
    req.role_name = 'student'
    next()
