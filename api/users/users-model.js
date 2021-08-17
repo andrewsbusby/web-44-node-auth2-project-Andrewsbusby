0dae1c05dde18389c776f8f154d54a0486451fa3
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-17 12:04:58 -0600

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,23 +1,8 @@
 const db = require('../../data/db-config.js');
 
 function find() {
-  /**
-    You will need to join two tables.
-    Resolves to an ARRAY with all users.
+  return db('users').select('user_id', 'username', 'role_name')
 
-    [
-      {
-        "user_id": 1,
-        "username": "bob",
-        "role_name": "admin"
-      },
-      {
-        "user_id": 2,
-        "username": "sue",
-        "role_name": "instructor"
-      }
-    ]
-   */
 }
 
 function findBy(filter) {
